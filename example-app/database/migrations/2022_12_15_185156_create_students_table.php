0816b046ed3a8a211bdffb6fc3771bef431b470a
Bernardo Biondini <bernardobiondini@gmail.com>
2022-12-15 15:55:44 -0300

    Criando a migration para criação da tabela de students

---
+++ example-app/database/migrations/2022_12_15_185156_create_students_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // dropping tables created automatically by laravel
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('personal_access_tokens');
+
+        // creating students table
+        Schema::create('students', function (Blueprint $table) {
+            $table->id(); // ja torna o id como primary-key
+            // $table->string('code', 30)->primary(); -> forma de tornar uma coluna primaria
+            $table->string('name');
+            $table->string('matricula')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
