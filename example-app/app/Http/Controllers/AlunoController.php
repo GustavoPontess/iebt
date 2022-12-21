a54d8b47398eb1a6606077872ad0736fc84ad8b7
Bernardo Biondini <bernardobiondini@gmail.com>
2022-12-21 09:52:30 -0300

    commenting code

--- example-app/app/Http/Controllers/AlunoController.php
+++ example-app/app/Http/Controllers/AlunoController.php
@@ -7,33 +7,40 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
+    /**
+     * Get all students Page
+     *
+     * Page for viewing all registered students
+     *
+     * @return View
+     */
+    public function ver():View
+    {
+        $students = Student::all();
+
+        return view("ver", ['students' => $students]);
+    }
+
+    /**
+     * Include Page
+     *
+     * Register page where the students are registered
+     *
+     * @return View
+     */
     public function incluir():View
     {
         return view("incluir");
     }
 
-    public function editar(int $id):View
-    {
-        $student = Student::findOrFail($id);
-
-        return view("editar", ['id' => $student->id, 'name' => $student->name, 'matricula' => $student->matricula]);
-    }
-
-    public function apagar(int $id):\Illuminate\Http\RedirectResponse
-    {
-        Student::where('id', $id)->delete();
-
-        return back();
-    }
-
-    public function ver():View
-    {
-        $students = Student::all();
-
-
-        return view("ver", ['students' => $students]);
-    }
-
+    /**
+     * Save student information.
+     *
+     * Take student information (Include Page) to save on the database
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function salvar(Request $request):\Illuminate\Http\RedirectResponse
     {
         $data = $request->query();
@@ -48,6 +55,29 @@ class AlunoController extends Controller
         return back();
     }
 
+    /**
+     * Edit student information page.
+     *
+     * Get a student show their information and allow edition
+     *
+     * @param  int  $id
+     * @return View
+     */
+    public function editar(int $id):View
+    {
+        $student = Student::findOrFail($id);
+
+        return view("editar", ['id' => $student->id, 'name' => $student->name, 'matricula' => $student->matricula]);
+    }
+
+    /**
+     * Update student information.
+     *
+     * Take de edited student information and update it on the database
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function alterar(Request $request):\Illuminate\Http\RedirectResponse
     {
         $data = $request->query();
@@ -56,4 +86,20 @@ class AlunoController extends Controller
 
         return back();
     }
+
+    /**
+     * Delete student information.
+     *
+     * Get a specific student and delete ir from the database
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function apagar(int $id):\Illuminate\Http\RedirectResponse
+    {
+        Student::where('id', $id)->delete();
+
+        return back();
+    }
+
 }
